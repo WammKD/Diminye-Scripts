d8485043e2e2c2e92f9991ca603a41f5831107cd
Sherlock Holmes <jaft.r@outlook.com>
2019-08-14 22:47:03 -0500

    Forgot the script that displays the weather in fullscreen and add some keybindings to launch the elements you can click on in the lemonbar.

---
+++ lemonbar/lemon_weather_launcher.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+curl https://wttr.in
+# wmctrl -r :ACTIVE: -b add,maximized_horz && wmctrl -r :ACTIVE: -b add,maximized_vert
+
+while
+  [ true ]
+do
+  read -t 3 -n 1
+
+  if
+    [ $? = 0 ]
+  then
+    exit
+  fi
+done
