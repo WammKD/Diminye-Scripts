c606f1ba5830c54c1dd1b7fc887a60ff5602a179
Sherlock Holmes <jaft.r@outlook.com>
2019-08-28 00:56:28 -0500

    Use the mapped volume percentage, for a more accurate capture of the status, and switch to a headphones icon when headphones are plugged in.

--- lemonbar/lemon_volume.sh
+++ lemonbar/lemon_volume.sh
@@ -3,37 +3,50 @@
 if
     [ "$1" = + ]
 then
-    amixer -c1 sset Master 3%+
+    amixer -M -c1 sset Master 3%+
 elif
     [ "$1" = - ]
 then
-    amixer -c1 sset Master 3%-
+    amixer -M -c1 sset Master 3%-
 elif
     [ "$1" = tog ]
 then
-    amixer -c1 sset Master toggle
+    amixer -M -c1 sset Master toggle
 fi
 
 
-data=$(amixer -c1 sget Master)
+data=$(amixer -M -c1 sget Master)
 level=$(echo $data | awk -F"[][]" '/dB/ { print $2 }')
 icon=$(if
            [ $(echo $data | awk -F"[][]" '/dB/ { print $6 }') = on ]
        then
-	   case $(echo $level | awk -v len=${#level} '{ string=substr($0, 1, len - 1); print string; }') in
-	       [0-9]|[1-2][0-9]|3[0-3])
-		   echo $(/usr/bin/printf '\ue04e')
-		   ;;
-	       3[4-9]|[4-5][0-9]|6[0-7])
-		   echo $(/usr/bin/printf '\ue050')
-		   ;;
-	       6[8-9]|[7-9][0-9]|100)
-		   echo $(/usr/bin/printf '\ue05d')
-		   ;;
-	       *)
-		   echo "ERROR"
-		   ;;
-	   esac
+	   if
+	       [ "$(amixer -M -c1 contents | grep -A2 Headphone\ Jack | grep values=o | cut -d = -f 2)" = "on" ]
+	   then
+	       case $(echo $level | awk -v len=${#level} '{ string=substr($0, 1, len - 1); print string; }') in
+		   [0-9]|[1-4][0-9])
+		       echo $(/usr/bin/printf '\ue0fc')
+		       ;;
+		   *)
+		       echo $(/usr/bin/printf '\ue0fd')
+		       ;;
+	       esac
+	   else
+	       case $(echo $level | awk -v len=${#level} '{ string=substr($0, 1, len - 1); print string; }') in
+		   [0-9]|[1-2][0-9]|3[0-3])
+		       echo $(/usr/bin/printf '\ue04e')
+		       ;;
+		   3[4-9]|[4-5][0-9]|6[0-7])
+		       echo $(/usr/bin/printf '\ue050')
+		       ;;
+		   6[8-9]|[7-9][0-9]|100)
+		       echo $(/usr/bin/printf '\ue05d')
+		       ;;
+		   *)
+		       echo "ERROR"
+		       ;;
+	       esac
+	   fi
        else
            echo $(/usr/bin/printf '\ue04f')
        fi)
