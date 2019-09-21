7647f469bf8a7fce3e846c24968f96404a09015e
WammKD <jaft.r@outlook.com>
2019-09-21 15:29:29 -0500

    `xfce4-power-manager` doesn't seem to have settings to notifications so let's add our own.
    Plus it separates out some of the logic and cleans up the code a
    little, reducing redundancy.

--- lemonbar/lemon_battery.sh
+++ lemonbar/lemon_battery.sh
@@ -3,51 +3,68 @@
 color_alert="#c03630"
 color_norm="#FFFFFF"
 color_charging="#87b158"
-stuff=$(acpi -b | while read -r batt
-                  do
-                      BATTERY_STATE=$(echo "${batt}" | grep -wo "Full\|Charging\|Discharging")
-                      BATTERY_POWER=$(echo "${batt}" | grep -o  '[0-9]\+%' | sed 's/%//g')
-
-                      if
-                          [ "${BATTERY_STATE}" = "Full" ] || [ "${BATTERY_POWER}" -eq 100 ]
-                      then
-                          result="%{U$color_charging}%{F$color_charging}$(/usr/bin/printf '\ue201')"
-                          percent="$BATTERY_POWER"
-                      else
-                          if
-                              [ "${BATTERY_POWER}" -le 10 ]
-                          then
-                              color="$color_alert"
-                          else
-                              color="$color_charging"
-                          fi
-
-                          if
-                              [ "${BATTERY_STATE}" = "Discharging" ]
-                          then
-                              percent="$BATTERY_POWER%%"
-                          else
-                              percent="$BATTERY_POWER$(/usr/bin/printf '\ue09e')"
-                          fi
 
 
+data=$(acpi -b)
 
-                          if
-                              [ "${BATTERY_POWER}" -le 33 ]
-                          then
-                              icon="$(/usr/bin/printf '\ue1fd')"
-                          elif
-                              [ "${BATTERY_POWER}" -le 67 ]
-                          then
-                              icon="$(/usr/bin/printf '\ue1fe')"
-                          else
-                              icon="$(/usr/bin/printf '\ue1ff')"
-                          fi
+BATTERY_STATE=$(echo "${data}" | grep -wo "Full\|Charging\|Discharging")
+BATTERY_POWER=$(echo "${data}" | grep -o  '[0-9]\+%' | sed 's/%//g')
 
-                          result="%{U$color}%{F$color} $icon"
-                      fi
+notify_p=$(if
+	      [ $(date '+%S')    -le $1            ] &&
+	      [ "$BATTERY_STATE"  =  "Discharging" ] &&
+	      [ $BATTERY_POWER   -le 10            ]
+	   then
+	       echo "true"
+	   else
+	       echo "false"
+	   fi)
 
-                      echo "$result %{F#FFFFFF}$percent"
-                  done)
+stuff=$(if
+            [ "${BATTERY_STATE}" = "Full" ] || [ "${BATTERY_POWER}" -eq 100 ]
+        then
+            result="%{U$color_charging}%{F$color_charging}$(/usr/bin/printf '\ue201')"
+            percent="$BATTERY_POWER"
+        else
+            if
+                [ "${BATTERY_POWER}" -le 10 ]
+            then
+                color="$color_alert"
+            else
+                color="$color_charging"
+            fi
+
+            if
+                [ "${BATTERY_STATE}" = "Discharging" ]
+            then
+                percent="$BATTERY_POWER%%"
+            else
+                percent="$BATTERY_POWER$(/usr/bin/printf '\ue09e')"
+            fi
+
+
+
+            if
+                [ "${BATTERY_POWER}" -le 33 ]
+            then
+                icon="$(/usr/bin/printf '\ue1fd')"
+            elif
+                [ "${BATTERY_POWER}" -le 67 ]
+            then
+                icon="$(/usr/bin/printf '\ue1fe')"
+            else
+                icon="$(/usr/bin/printf '\ue1ff')"
+            fi
+
+            result="%{U$color}%{F$color} $icon"
+        fi
+
+        echo "$result %{F#FFFFFF}$percent")
+
+if
+    [ "$notify_p" = "true" ]
+then
+    notify-send "Low battery;$(echo $data | cut -d , -f 3)." -i notification-battery-low
+fi
 
 echo "battery%{+u}$stuff %{-u}" > "/tmp/lemon/panel_fifo"
