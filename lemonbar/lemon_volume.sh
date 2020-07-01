28dc7a890c2c8c25ca2297607cf3357f4dddfe3f
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-07-01 03:13:27 -0500

    Clean up some of this indentation…

--- lemonbar/lemon_volume.sh
+++ lemonbar/lemon_volume.sh
@@ -20,37 +20,37 @@ sleep 0.03
 data=$(amixer -M -c1 sget Master)
 level=$(echo $data | awk -F"[][]" '/dB/ { print $2 }')
 icon=$(if
-           [ $(echo $data | awk -F"[][]" '/dB/ { print $6 }') = on ]
+       	[ $(echo $data | awk -F"[][]" '/dB/ { print $6 }') = on ]
        then
-	   if
-	       [ "$(amixer -M -c1 contents | grep -A2 Headphone\ .*Jack | grep values=o | cut -d = -f 2)" = "on" ]
-	   then
-	       case $(echo $level | awk -v len=${#level} '{ string=substr($0, 1, len - 1); print string; }') in
-		   [0-9]|[1-4][0-9])
-		       echo $(/usr/bin/printf '\ue0fc')
-		       ;;
-		   *)
-		       echo $(/usr/bin/printf '\ue0fd')
-		       ;;
-	       esac
-	   else
-	       case $(echo $level | awk -v len=${#level} '{ string=substr($0, 1, len - 1); print string; }') in
-		   [0-9]|[1-2][0-9]|3[0-3])
-		       echo $(/usr/bin/printf '\ue04e')
-		       ;;
-		   3[4-9]|[4-5][0-9]|6[0-7])
-		       echo $(/usr/bin/printf '\ue050')
-		       ;;
-		   6[8-9]|[7-9][0-9]|100)
-		       echo $(/usr/bin/printf '\ue05d')
-		       ;;
-		   *)
-		       echo "ERROR"
-		       ;;
-	       esac
-	   fi
+       	if
+       		[ "$(amixer -M -c1 contents | grep -A2 Headphone\ .*Jack | grep values=o | cut -d = -f 2)" = "on" ]
+       	then
+       		case $(echo $level | awk -v len=${#level} '{ string=substr($0, 1, len - 1); print string; }') in
+       			[0-9]|[1-4][0-9])
+       				echo $(/usr/bin/printf '\ue0fc')
+       				;;
+       			*)
+       				echo $(/usr/bin/printf '\ue0fd')
+       				;;
+       		esac
+       	else
+       		case $(echo $level | awk -v len=${#level} '{ string=substr($0, 1, len - 1); print string; }') in
+       			[0-9]|[1-2][0-9]|3[0-3])
+       				echo $(/usr/bin/printf '\ue04e')
+       				;;
+       			3[4-9]|[4-5][0-9]|6[0-7])
+       				echo $(/usr/bin/printf '\ue050')
+       				;;
+       			6[8-9]|[7-9][0-9]|100)
+       				echo $(/usr/bin/printf '\ue05d')
+       				;;
+       			*)
+       				echo "ERROR"
+       				;;
+       		esac
+       	fi
        else
-           echo $(/usr/bin/printf '\ue04f')
+       	echo $(/usr/bin/printf '\ue04f')
        fi)
 
 if
