301f718bbacef19c2eab738c3ce5bf026515e16a
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-07-01 23:29:33 -0500

    Fix indentation for this script.

--- lemonbar/lemon_brightness.sh
+++ lemonbar/lemon_brightness.sh
@@ -7,47 +7,47 @@ max=$(cat $max_file)
 current=$(cat $current_file)
 
 if
-    [ "$1" = + ]
+	[ "$1" = + ]
 then
-    result=$((current + $increment))
+	result=$((current + $increment))
 
-    if
-	[ $result -gt $max ]
-    then
-	echo $max    | tee $current_file
-    else
-	echo $result | tee $current_file
-    fi
+	if
+		[ $result -gt $max ]
+	then
+		echo $max    | tee $current_file
+	else
+		echo $result | tee $current_file
+	fi
 elif
-    [ "$1" = - ]
+	[ "$1" = - ]
 then
-    result=$((current - $increment))
+	result=$((current - $increment))
 
-    if
-	[ $result -lt 0 ]
-    then
-	echo 0       | tee $current_file
-    else
-	echo $result | tee $current_file
-    fi
+	if
+		[ $result -lt 0 ]
+	then
+		echo 0       | tee $current_file
+	else
+		echo $result | tee $current_file
+	fi
 fi
 
 
 
 percent=$(echo "scale = 2; (($(cat $current_file) / $max) * 100)" | bc | cut -d . -f 1)
 icon=$(case $percent in
-	   [0-9]|[1-2][0-9]|3[0-3])
-	       echo $(/usr/bin/printf '\ue1bc')
-	       ;;
-	   3[4-9]|[4-5][0-9]|6[0-7])
-	       echo $(/usr/bin/printf '\ue1c3')
-		   ;;
-	   6[8-9]|[7-9][0-9]|100)
-	       echo $(/usr/bin/printf '\ue1c2')
-	       ;;
-	   *)
-	       echo "ERROR"
-	       ;;
+       	[0-9]|[1-2][0-9]|3[0-3])
+       		echo $(/usr/bin/printf '\ue1bc')
+       		;;
+       	3[4-9]|[4-5][0-9]|6[0-7])
+       		echo $(/usr/bin/printf '\ue1c3')
+       		;;
+       	6[8-9]|[7-9][0-9]|100)
+       		echo $(/usr/bin/printf '\ue1c2')
+       		;;
+       	*)
+       		echo "ERROR"
+       		;;
        esac)
 
 
